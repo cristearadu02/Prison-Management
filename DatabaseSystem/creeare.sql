6169ef74805291f82a9f688171429ff6c20788e6
Radu <93822107+cristearadu02@users.noreply.github.com>
2023-05-28 10:23:05 +0300

    Update database

--- DatabaseSystem/creeare.sql
+++ DatabaseSystem/creeare.sql
@@ -26,7 +26,7 @@ CREATE TABLE Detinuti (
 
 -- Crearea tabelului Vizite
 CREATE TABLE Vizite (
-  id INT AUTO_INCREMENT PRIMARY KEY,vizitatori
+  id INT AUTO_INCREMENT PRIMARY KEY,
   id_vizitator INT,
   id_detinut INT,
   motiv_vizita TEXT,
@@ -34,3 +34,23 @@ CREATE TABLE Vizite (
   FOREIGN KEY (id_vizitator) REFERENCES Vizitatori(id),
   FOREIGN KEY (id_detinut) REFERENCES Detinuti(id)
 );
+
+-- Adaugarea campului 'role' in tabela 'Vizitatori'
+ALTER TABLE Vizitatori
+ADD role VARCHAR(255) DEFAULT 'user';
+
+-- Adaugarea campului 'relatia' in tabela 'Vizite'
+ALTER TABLE Vizite
+ADD relatia VARCHAR(255);
+
+-- Adaugarea campului 'natura' in tabela 'Vizite'
+ALTER TABLE Vizite
+ADD natura VARCHAR(255);
+
+-- Adaugarea campului 'obiecte_aduse' in tabela 'Vizite'
+ALTER TABLE Vizite
+ADD obiecte_aduse VARCHAR(255);
+
+-- Adaugarea campului 'martori' in tabela 'Vizite'
+ALTER TABLE Vizite
+ADD martori VARCHAR(255) NULL;
--- DatabaseSystem/populare.sql
+++ DatabaseSystem/populare.sql
@@ -1,34 +1,37 @@
 USE projectweb;
 
--- Populating the Vizitatori table
-INSERT INTO Vizitatori (nume, prenume, cnp, parola, email, numar_telefon, data_nasterii, cetatenie, intrebare_securitate, raspuns_intrebare_securitate)
+-- Popularea tabelului Vizitatori
+INSERT INTO Vizitatori (nume, prenume, cnp, parola, email, numar_telefon, data_nasterii, cetatenie, intrebare_securitate, raspuns_intrebare_securitate, role)
 VALUES
-  ('John', 'Doe', '1234567890123', 'password1', 'john@example.com', '1234567890', '1990-01-01', 'Romanian', 'Question 1', 'Answer 1'),
-  ('Jane', 'Smith', '9876543210987', 'password2', 'jane@example.com', '9876543210', '1995-05-10', 'American', 'Question 2', 'Answer 2'),
-  ('Alice', 'Johnson', '4567890123456', 'password3', 'alice@example.com', '4567890123', '1988-07-15', 'British', 'Question 3', 'Answer 3'),
-  ('Bob', 'Anderson', '7890123456789', 'password4', 'bob@example.com', '7890123456', '1992-12-25', 'Canadian', 'Question 4', 'Answer 4'),
-  ('Eva', 'Brown', '2345678901234', 'password5', 'eva@example.com', '2345678901', '1998-03-20', 'German', 'Question 5', 'Answer 5'),
-  ('Michael', 'Wilson', '5678901234567', 'password6', 'michael@example.com', '5678901234', '1993-09-05', 'Australian', 'Question 6', 'Answer 6'),
-  ('Sophia', 'Taylor', '8901234567890', 'password7', 'sophia@example.com', '8901234567', '1991-11-30', 'French', 'Question 7', 'Answer 7');
+  ('Popescu', 'Ion', '12345678901234', 'parola1', 'popescu.ion@example.com', '1234567890', '1990-01-01', 'Romanian', 'Care este numele mamei tale?', 'Mama', 'admin'),
+  ('Ionescu', 'Maria', '98765432109876', 'parola2', 'maria.ionescu@example.com', '0987654321', '1995-05-10', 'Romanian', 'Care este numele primului tau animal de companie?', 'Rex', 'user'),
+  ('Dumitrescu', 'Andrei', '45678901234567', 'parola3', 'andrei.dumitrescu@example.com', '5555555555', '1985-12-15', 'Romanian', 'Care este filmul tau preferat?', 'Inception', 'user'),
+  ('Mihai', 'Ana', '98765432101234', 'parola4', 'ana.mihai@example.com', '7777777777', '1992-06-20', 'Romanian', 'Care este numele primului tau profesor?', 'Popescu', 'user'),
+  ('Popa', 'Alexandru', '12345678909876', 'parola5', 'alexandru.popa@example.com', '9999999999', '1988-03-25', 'Romanian', 'Care este culoarea ta preferata?', 'Albastru', 'user'),
+  ('Stancu', 'Elena', '23456789098765', 'parola6', 'elena.stancu@example.com', '2222222222', '1980-09-08', 'Romanian', 'Care este destinatia ta de vacanta preferata?', 'Bali', 'user'),
+  ('Radu', 'Adrian', '98765432123456', 'parola7', 'adrian.radu@example.com', '3333333333', '1982-07-12', 'Romanian', 'Care este numele primului tau coleg de clasa?', 'Marius', 'user'),
+  ('Constantin', 'Mirela', '34567890123456', 'parola8', 'mirela.constantin@example.com', '4444444444', '1998-11-03', 'Romanian', 'Care este numele orasului in care te-ai nascut?', 'Bucuresti', 'user');
 
--- Populating the Detinuti table
+-- Popularea tabelului Detinuti
 INSERT INTO Detinuti (nume, prenume, cnp, motivul_detinerii, gradul)
 VALUES
-  ('Smith', 'John', '1234567890123', 'Theft', 3),
-  ('Johnson', 'Michael', '9876543210987', 'Assault', 2),
-  ('Brown', 'Sarah', '4567890123456', 'Drug possession', 1),
-  ('Davis', 'Emily', '7890123456789', 'Robbery', 4),
-  ('Anderson', 'James', '2345678901234', 'Fraud', 3),
-  ('Wilson', 'Jessica', '5678901234567', 'Burglary', 2),
-  ('Taylor', 'Daniel', '8901234567890', 'Murder', 5);
+  ('Popescu', 'Mihai', '11111111111111', 'Furt', 2),
+  ('Ionescu', 'Gabriel', '22222222222222', 'Omucidere', 4),
+  ('Dumitru', 'Andrei', '33333333333333', 'Trafic de droguri', 3),
+  ('Popa', 'Maria', '44444444444444', 'Furt', 1),
+  ('Georgescu', 'Cristian', '55555555555555', 'Viol', 2),
+  ('Stanescu', 'Alexandru', '66666666666666', 'Trafic de persoane', 4),
+  ('Radu', 'Elena', '77777777777777', 'Furt', 1),
+  ('Constantinescu', 'Marius', '88888888888888', 'Frauda', 3);
 
--- Populating the Vizite table
-INSERT INTO Vizite (id_vizitator, id_detinut, motiv_vizita, dataa)
+-- Popularea tabelului Vizite
+INSERT INTO Vizite (id_vizitator, id_detinut, motiv_vizita, dataa, relatia, natura, obiecte_aduse, martori)
 VALUES
-  (1, 1, 'Family visit', '2023-01-01'),
-  (2, 2, 'Legal consultation', '2023-02-15'),
-  (3, 3, 'Friend visit', '2023-03-10'),
-  (4, 4, 'Medical check-up', '2023-04-05'),
-  (5, 5, 'Family visit', '2023-05-20'),
-  (6, 6, 'Legal consultation', '2023-06-25'),
-  (7, 7, 'Friend visit', '2023-07-15');
+  (1, 1, 'Vizita de familie', '2023-05-01', 'frate', 'personal', 'cadouri', NULL),
+  (2, 2, 'Vizita de prietenie', '2023-05-02', 'prieten', 'profesional', 'scrisori', 'Maria'),
+  (3, 3, 'Vizita avocatiala', '2023-05-03', 'avocat', 'legal', 'documente', 'Alexandru'),
+  (4, 4, 'Vizita de familie', '2023-05-04', 'mama', 'personal', 'alimente', NULL),
+  (5, 5, 'Vizita de prietenie', '2023-05-05', 'prieten', 'personal', 'flori', 'Mihai'),
+  (6, 6, 'Vizita avocatiala', '2023-05-06', 'avocat', 'legal', 'contracte', 'Gabriel'),
+  (7, 7, 'Vizita de familie', '2023-05-07', 'sora', 'personal', 'haine', NULL),
+  (8, 8, 'Vizita de prietenie', '2023-05-08', 'prieten', 'personal', 'jucarii', 'Elena');
