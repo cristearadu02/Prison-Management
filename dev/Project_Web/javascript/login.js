1dcb8c10e894cdd9144538c551af0ccb376fb411
RobertMarian20 <100293849+RobertMarian20@users.noreply.github.com>
2023-06-11 23:46:42 +0300

    Add files via upload

--- dev/Project_Web/javascript/login.js
+++ dev/Project_Web/javascript/login.js
@@ -1,3 +1,51 @@
-function navigateToLink(path) {
-    window.location.href = path;
-  }
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get the button element inside the login form
+  const loginButton = document.querySelector('button');
+
+  loginButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const cnpInput = document.getElementById('CNP');
+    const passwordInput = document.getElementById('password');
+
+    const loginData = {
+      cnp: cnpInput.value,
+      password: passwordInput.value,
+    };
+
+    // Send the login data to the server
+    fetch('http://localhost:3000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(loginData),
+    })
+      .then(response => {
+        if (response.ok) {
+          // Successful login
+          return response.json();
+        } else {
+          // Login failed
+          throw new Error('Login failed. Please check your credentials.');
+        }
+      })
+      .then(data => {
+        // Handle the response data after successful login
+        console.log(data);
+
+        // Store the JWT in a secure cookie
+        document.cookie = `jwt=${data.token}; Secure; SameSite=Strict;`;
+        console.log(data.token);
+
+        window.location.href = './IndexLogat.html'; // Redirect to the logged-in page
+      })
+      .catch(error => {
+        // Handle any errors that occurred during the login process
+        console.error('Error during login:', error);
+        // Display an error message or perform error handling
+        alert('Login failed. Please check your credentials.');
+      });
+  });
+});
--- dev/Project_Web/javascript/register.js
+++ dev/Project_Web/javascript/register.js
@@ -1,24 +1,83 @@
 function previewImage(event) {
-    const reader = new FileReader();
-    const imagePreview = document.getElementById('image-preview');
-  
-    
-    reader.onload = function () {
-      if (reader.readyState === 2) {
-        imagePreview.src = reader.result;
-        imagePreview.style.display = 'block';
-      }
-    }
-  
+  const reader = new FileReader();
+  const imagePreview = document.getElementById('image-preview');
 
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      reader.readAsDataURL(file);
+  
+  reader.onload = function () {
+    if (reader.readyState === 2) {
+      imagePreview.src = reader.result;
+      imagePreview.style.display = 'block';
     }
   }
 
 
-  function navigateToLink(path) {
-    window.location.href = path;
+  if (event.target.files.length > 0) {
+    const file = event.target.files[0];
+    reader.readAsDataURL(file);
   }
+}
+
+
+function navigateToLink(path) {
+  window.location.href = path;
+}
+
+
+
+document.getElementById('register-form').addEventListener('submit', function(event) {
+  // Prevent the form from submitting normally
+  event.preventDefault();
+
+  // Create an object to hold the form data
+  let formData = {
+    //image: document.getElementById('image-upload').value,
+    cnp: document.getElementById('cnp').value,
+    nume: document.getElementById('family-name').value,
+    prenume: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    numar_telefon: document.getElementById('phone').value,
+    address: document.getElementById('address').value,
+    data_nasterii: document.getElementById('dob').value,
+    cetatenie: document.getElementById('citizenship').value,
+    password: document.getElementById('password').value,
+    confirmPassword: document.getElementById('confirm-password').value,
+    intrebare_securitate: document.getElementById('security-question').value,
+    raspuns_intrebare_securitate: document.getElementById('security-answer').value,
+    terms: document.querySelector('input[name="terms"]').checked,
+  };
+
+  // Send a POST request to the server
+  fetch('http://localhost:3000/api/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  })
+  .then(response => {
+    // Here you can check the response status
+    if (response.status === 201) {
+      // If the user is created successfully, you can redirect here
+      window.location.href = "./IndexLogat.html";
+    } else {
+      // If there was an error, handle it here
+      alert("Registration failed. An error occurred.");
+      console.error("Error during registration:", response.statusText);
+    }
+
+    return response.json();  // This will pass the JSON response to the next then()
+  })
+  .then(data => {
+    // Handle the response data
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle the error
+    alert("Registration failed. An error occurred.");
+    console.error("Error during registration:", error);
+  });
+});
+
+
+
 
